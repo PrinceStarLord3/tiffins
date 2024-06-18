9b8fe8ff8e3901ed82b329dd758fada4b5fdd259
PrinceStarLord3 <pavanpatilbro2020@gmail.com>
2024-06-18 16:32:48 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,10 @@
+# config.py
+
+# MongoDB connection
+MONGO_URI = 'mongodb+srv://tpadir02:UP9MKn8WrFBnuTZs@cluster0.p71easz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
+
+#Clster = Cluster0
+# 
+# Server details
+FQDN = 'your_server_ip'
+PORT = 5000  # Example port, adjust as needed
